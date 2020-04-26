6e9e11a87dd9d5e47386283a6a1f0509bd604477
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2020-04-26 19:31:37 +0200

    Updates

---
+++ precache-manifest.511a1dc3ca945f574d31a39a6b6594ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21831d3b44442d59963f502d3406a2dd",
+    "url": "/blokkeren/index.html"
+  },
+  {
+    "revision": "bf8dfb4be21b2aead9db",
+    "url": "/blokkeren/static/css/main.77670686.chunk.css"
+  },
+  {
+    "revision": "21fb53d2a3914e57735e",
+    "url": "/blokkeren/static/js/2.ed0f8aeb.chunk.js"
+  },
+  {
+    "revision": "2d4e50db9bebc1822939159016933eb5",
+    "url": "/blokkeren/static/js/2.ed0f8aeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf8dfb4be21b2aead9db",
+    "url": "/blokkeren/static/js/main.dffa4ad6.chunk.js"
+  },
+  {
+    "revision": "385edfad52e10590d879",
+    "url": "/blokkeren/static/js/runtime-main.f6b1fe95.js"
+  }
+]);
